bf27652bb691fd42e3047ff7aa648e7500f81d68
valeriagili <valerial.gili@gmail.com>
2015-12-10 15:51:42 +0100

    plot4 added

--- plot2.R
+++ plot2.R
@@ -1,9 +1,11 @@
 ## Download and unzip data
 if(!file.exists("./data")){dir.create("./data")}
-fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileURL, destfile="./data/household_power_consumption.zip", method = "curl")
-unzip("./data/household_power_consumption.zip", exdir = "./data/")
-        
+if(!file.exists("./data/household_power_consumption.txt")){
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, destfile="./data/household_power_consumption.zip", method = "curl")
+        unzip("./data/household_power_consumption.zip", exdir = "./data/")
+}
+
 ## Load data into R
 col_names <- read.table("./data/household_power_consumption.txt", sep = ";", 
                         nrows = 1, stringsAsFactors = FALSE)
--- plot4.R
+++ plot4.R
@@ -1,9 +1,11 @@
 ## Download and unzip data
 if(!file.exists("./data")){dir.create("./data")}
-fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileURL, destfile="./data/household_power_consumption.zip", method = "curl")
-unzip("./data/household_power_consumption.zip", exdir = "./data/")
-        
+if(!file.exists("./data/household_power_consumption.txt")){
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, destfile="./data/household_power_consumption.zip", method = "curl")
+        unzip("./data/household_power_consumption.zip", exdir = "./data/")
+}
+
 ## Load data into R
 col_names <- read.table("./data/household_power_consumption.txt", sep = ";", 
                         nrows = 1, stringsAsFactors = FALSE)
@@ -14,13 +16,25 @@ hpc_data <- read.table("./data/household_power_consumption.txt", header = FALSE,
 ## Convert dates and times to Date objects
 hpc_data$datetime <- strptime(paste(hpc_data$Date,hpc_data$Time), format = "%d/%m/%Y %H:%M:%S")
 
-## Open PNG device: create "plot3.png" of 480 pixels times 480 pixels (default values)
+## Open PNG device: create "plot4.png" of 480 pixels times 480 pixels (default values)
 #  in working directory
-png(filename = "plot2.png")
+png(filename = "plot4.png")
 
-## Create histogram
-with(hpc_data, plot(datetime, Global_active_power, type = "l", 
-                    xlab = "", ylab = "Global Active Power (kilowatts)"))
+## Generate layout
+par(mfrow= c(2,2))
+
+## Create histograms
+with(hpc_data, {
+     plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+     plot(datetime, Voltage, type = "l")
+     plot(datetime, Sub_metering_1, type = "l",
+                              xlab = "", ylab = "Energy sub metering")
+        points(datetime, Sub_metering_2, type = "l", col = "red")
+        points(datetime, Sub_metering_3, type = "l", col = "blue")
+        legend("topright", bty = "n", lty = "solid", col = c("black", "red", "blue"),  
+               legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(datetime, Global_reactive_power, type = "l")
+               })
 
 ## Close the PNG file device
 dev.off()
